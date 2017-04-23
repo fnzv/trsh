964af3c8ea527a48306cfc4db9ca2f0a25cb08a1
Sami <fnzv@users.noreply.github.com>
2017-04-23 13:48:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Downloading & Installing Packages.. \n"
-apt-get install -y python supervisor nmap dig mtr python-pip && pip install telepot
+apt-get install -y python supervisor nmap dnsutils mtr python-pip && pip install telepot
 
 echo "Configuring tgrsh as a service.. \n"
 scp supervisor/conf.d/tgrsh.conf /etc/supervisor/conf.d/tgrsh.conf
