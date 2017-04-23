ff31b4961e98f902afc3c7c028b687c2099ca87d
Sami <fnzv@users.noreply.github.com>
2017-04-23 17:59:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ apt-get install -y python supervisor nmap dnsutils mtr python-pip && pip install
 echo "Configuring trsh as a service.. \n"
 scp supervisor/conf.d/trsh.conf /etc/supervisor/conf.d/trsh.conf
 
-echo "Starting trsh.. \n"
-supervisorctl update && supervisorctl restart trsh
+echo "Update supervisord.. \n"
+supervisorctl update 
 
+echo "Edit your Telegram Token and Sender id then start the service with supervisorctl start trsh.."
