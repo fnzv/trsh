c184cd0711a7551d44d0e682228b9060b3ff9bb2
Sami <fnzv@users.noreply.github.com>
2017-04-24 23:35:38 +0200

    Update install.sh
    Added Token & Sender id input

--- install.sh
+++ install.sh
@@ -2,10 +2,23 @@
 echo "Downloading & Installing Packages.. \n"
 apt-get install -y python supervisor nmap dnsutils mtr python-pip && pip install telepot
 
+echo "Enter your Telegram BOT Token: "
+read -sr TG_BOT_TOKEN
+
+
+echo "Enter your Telegram Sender ID: "
+read -sr SENDER_ID
+
+sed -i s"/SENDER-ID-LIST/$SENDER_ID/" controller.py
+
+sed -i s"/TG-BOT-TOKEN/$TG_BOT_TOKEN/" controller.py
+
+
 echo "Configuring trsh as a service.. \n"
 scp supervisor/conf.d/trsh.conf /etc/supervisor/conf.d/trsh.conf
 
 echo "Update supervisord.. \n"
 supervisorctl update 
 
-echo "Edit your Telegram Token and Sender id then start the service with supervisorctl start trsh.."
+echo "Starting trsh service.. \n"
+supervisorctl start trsh
